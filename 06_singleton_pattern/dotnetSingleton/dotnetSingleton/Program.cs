03cc0c62b7620898f834f2bf2448f5c6178ed1c5
Jakub Plesnik <jakub.plesnik@gmail.com>
2022-05-13 11:17:02 +0200

    Add singleton in dotnet

---
+++ 06_singleton_pattern/dotnetSingleton/dotnetSingleton/Program.cs
@@ -0,0 +1,9 @@
+﻿using dotnetSingleton;
+
+Console.WriteLine("Singleton Example");
+
+var instanceA = Singleton.GetInstance();
+var instanceB = Singleton.GetInstance();
+
+var areSame  = instanceA.Equals(instanceB);
+Console.WriteLine($"Instances are the same: {areSame}");
---
+++ 06_singleton_pattern/dotnetSingleton/dotnetSingleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace dotnetSingleton;
+
+public class Singleton
+{
+    private Singleton() { }
+
+    private static Singleton? instance;
+
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
